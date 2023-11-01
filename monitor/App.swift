d43d9c047412da8224d47fe1b8d3ef94e400b071
blabla-yy <yahaha7777@gmail.com>
2023-11-01 16:07:41 +0800

    window

---
+++ monitor/App.swift
@@ -0,0 +1,9 @@
+//
+//  App.swift
+//  NetworkMonitor
+//
+//  Created by wyy on 2023/11/1.
+//  Copyright © 2023 yahaha. All rights reserved.
+//
+
+import Foundation
---
+++ monitor/views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  NetworkMonitor
+//
+//  Created by wyy on 2023/10/31.
+//  Copyright © 2023 yahaha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
